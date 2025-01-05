d0d82e913ba91b355ab32254881d5087f7b7132d
stan <lovesimy@163.com>
2025-01-05 21:14:55 +0800

    prepare domain writer

--- examples/carnival/src/main/kotlin/io/kofa/example/carnival/application/ClownModule.kt
+++ examples/carnival/src/main/kotlin/io/kofa/example/carnival/application/ClownModule.kt
@@ -2,11 +2,13 @@ package io.kofa.example.carnival.application
 
 import io.kofa.example.carnival.business.handler.ClownHandler
 import io.kofa.example.carnival.generated.CarnivalMessageHandler
+import io.kofa.platform.api.annotation.DomainModule
 import io.kofa.platform.api.config.Config
 import io.kofa.platform.api.inject.ComponentModuleDeclaration
 import org.koin.core.qualifier.named
 import org.koin.dsl.bind
 
+@DomainModule("Clown", ClownHandler::class)
 object ClownModule {
     fun clownConfig(): ComponentModuleDeclaration = {
         scoped {
@@ -20,6 +22,7 @@ object ClownModule {
         }.bind()
     }
 
+    //TODO: remove once DomainModule annotation enabled
     fun clownHandler(): ComponentModuleDeclaration = {
         scoped {
             ClownHandler(get())
--- platform/platform-api/src/main/kotlin/io/kofa/platform/api/annotation/DomainModule.kt
+++ platform/platform-api/src/main/kotlin/io/kofa/platform/api/annotation/DomainModule.kt
@@ -1,4 +1,6 @@
 package io.kofa.platform.api.annotation
 
+import kotlin.reflect.KClass
+
 @Target(AnnotationTarget.CLASS, AnnotationTarget.TYPE)
-annotation class DomainModule(val componentType: String)
+annotation class DomainModule(val componentType: String, val handlerClass: KClass<*>)
